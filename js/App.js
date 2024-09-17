eb8c437e1b9d6845910dd4db86aa6d3f15b1644b
MaksymKushnir <maxim140296@gmail.com>
2024-09-17 17:15:55 +0300

    start proj

--- js/App.js
+++ js/App.js
@@ -1,14 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     const button = document.createElement('button');
-    button.textContent = 'Змінити побажання';
-    button.classList.add('btn', 'btn-primary', 'mt-3');
+    //button.textContent = 'Змінити листівку';
+    button.classList.add('btn', 'btn-light', 'mt-3');
     document.querySelector('.card-body').appendChild(button);
 
     button.addEventListener('click', function() {
         const wishes = [
-            "Бажаю тобі щастя, здоров'я та успіхів у всіх починаннях!",
-            "Нехай всі твої мрії здійсняться!",
-            "Бажаю тобі багато радості та любові!"
+            //"Бажаю тобі щастя, здоров'я та успіхів у всіх починаннях!",
+            //"Нехай всі твої мрії здійсняться!",
+            //"Бажаю тобі багато радості та любові!"
         ];
         const randomWish = wishes[Math.floor(Math.random() * wishes.length)];
         document.querySelector('.card-text').textContent = randomWish;
